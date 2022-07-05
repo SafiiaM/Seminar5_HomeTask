fc7fe753b0b9ad09dc526db1e79d145dc3852ed4
Safiia <sonymamedova@gmail.com>
2022-07-05 17:56:07 +0300

    add Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -3,3 +3,21 @@
 
 // [345, 897, 568, 234] -> 2
 
+int[] array = new int[4];
+int countChet = 0;
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 1000);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+for (int k = 0; k < array.Length; k++)
+{
+    if (array[k] % 2 == 0) countChet = countChet + 1;
+    
+}
+Console.Write("Количество четных чисел в массиве -> " + countChet);
+Console.WriteLine();
